9fb63e373790ff12f35870708467379f6acb47f5
tuf_unkn0wn <40415158+unkn0wnh4ckr@users.noreply.github.com>
2019-08-26 10:37:11 -0400

    Delete dragonupdate.py

--- dragonupdate.py
+++
@@ -1,4 +0,0 @@
-import os
-print "UPDATING..."
-os.system("cd")
-os.system('cd /root/ && rm -fr hackers-tool-kit && git clone https://github.com/unkn0wnh4ckr/dragonscan && echo "[UPDATED]:  Restart Your Terminal"')
