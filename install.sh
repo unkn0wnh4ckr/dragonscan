b22e22202228b730dbd908743be0fb4356d1c87f
tuf_unkn0wn <40415158+unkn0wnh4ckr@users.noreply.github.com>
2019-08-26 10:53:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 printf "INSTALLING..."
 apt update
-apt install pip
 pip install pyinstaller
 pip install socket
 pip install requests
@@ -10,10 +9,10 @@ apt install aircrack-ng -y
 apt install netdiscover -y
 apt install nmap -y
 apt install curl -y
-pyinstaller --onefile /root/dragonscan/script/dragonscan.py
-pyinstaller --onefile /root/dragonscan/script/dragonupdate.py
+cd /root/dragonscan/script && pyinstaller --onefile dragonscan.py
+cd /root/dragonscan/script && pyinstaller --onefile dragonupdate.py
 cp -fr /root/dragonscan/script/dist/dragonscan /bin
 cp -fr /root/dragonscan/script/dist/dragonupdate /bin
-printf "[DONE]"
-printf 'type "dragonupdate" anywhere in your terminal to update the script'
+printf "[DONE]\n"
+printf 'type "dragonupdate" anywhere in your terminal to update the script\n'
 printf 'type "dragonscan" anywhere in your terminal to run the script'
