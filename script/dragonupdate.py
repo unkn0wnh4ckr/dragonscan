eccbf6708852cb7af6c2679271c87207edd935aa
tuf_unkn0wn <40415158+unkn0wnh4ckr@users.noreply.github.com>
2019-08-26 11:24:20 -0400

    Update dragonupdate.py

--- script/dragonupdate.py
+++ script/dragonupdate.py
@@ -1,6 +1,6 @@
 import os
 print "UPDATING..."
-os.system("rm /bin/dragonscan")
-os.system("rm /bin/dragonupdate")
+os.system("cd /bin && rm dragonscan")
+os.system("cd /bin && rm dragonupdate")
 os.system("cd")
 os.system('cd /root/ && rm -fr hackers-tool-kit && git clone https://github.com/unkn0wnh4ckr/dragonscan && echo "[UPDATED]:  Restart Your Terminal And Run install.sh Again"')
